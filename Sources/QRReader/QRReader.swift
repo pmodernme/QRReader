73e592eaee818e903c97e04829765678ddc93af9
Zoe Van Brunt <zoe@zoelog.io>
2023-08-07 12:59:04 -0700

    initial commit

--- Sources/QRReader/QRReader.swift
+++ Sources/QRReader/QRReader.swift
@@ -1,6 +1,32 @@
-public struct QRReader {
-    public private(set) var text = "Hello, World!"
+//
+//  QRReader.swift
+//  
+//
+//  Created by Zoe Van Brunt on 11/11/22.
+//
 
-    public init() {
+import SwiftUI
+
+public struct QRReader: UIViewRepresentable {
+    public init(scanResults: Binding<Set<String>>) {
+        _scanResults = scanResults
+    }
+    
+    @Binding public var scanResults: Set<String>
+    
+    public func updateUIView(_ view: QRReaderView, context: Context) { }
+    
+    public func makeUIView(context: Context) -> QRReaderView {
+        let view = QRReaderView()
+        view.videoPreviewLayer.videoGravity = .resizeAspectFill
+        view.start()
+        view.onReaderDidReadString = {
+            scanResults = $0
+        }
+        return view
+    }
+    
+    public static func dismantleUIView(_ view: QRReaderView, coordinator: ()) {
+        view.stop()
     }
 }
--- Tests/QRReaderTests/QRReaderTests.swift
+++ Tests/QRReaderTests/QRReaderTests.swift
@@ -6,6 +6,6 @@ final class QRReaderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(QRReader().text, "Hello, World!")
+//        XCTAssertEqual(QRReader().text, "Hello, World!")
     }
 }
